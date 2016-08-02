f8d4bcb9a0968f72d3488e1b14fd0e888925bdcf
mirrayees <mirrayees859@gmail.com>
2016-08-02 09:32:43 +0530

    common header and fields added

--- payumoney.js
+++ payumoney.js
@@ -2,10 +2,10 @@ var request = require('request'),
 	queryString = require('querystring'),
 	crypto = require('crypto');
 
-var BASE_URL= 'https://test.payumoney.com';
-var CREATE_PAYEMENT= 'https://test.payu.in/_payment';
+var BASE_URL = 'https://test.payumoney.com';
+var CREATE_PAYEMENT = 'https://test.payu.in/_payment';
 
-var ENDPOINTS = {		
+var ENDPOINTS = {
 	'PAYEMENT_DETAILS': '/payment/op/getPaymentResponse?',
 	'PAYMENT_STATUS': '/payment/payment/chkMerchantTxnStatus?',
 	'INITIATE_REFUND': '/payment/merchant/refundPayment?',
@@ -23,23 +23,21 @@ module.exports = {
 		'accept': ''
 	},
 	hashSequence: "key|txnid|amount|productinfo|firstname|email|udf1|udf2|udf3|udf4|udf5|udf6|udf7|udf8|udf9|udf10",
-	setHeaders: function(auth, contentLength, contentData) {
+	setHeaders: function(auth) {
 		this.HEADERS['Authorization'] = auth;
 		this.HEADERS['Content-Type'] = 'application/json';
-		this.HEADERS['Content-Length'] = contentLength;
-		this.HEADERS['content'] = contentData;
 		this.HEADERS['accept'] = '*/*';
 	},
-	caller: function(url, method, header, callback) {
+	caller: function(url, method, callback) {
 		request({
 			method: method,
 			url: url,
-			headers: header
+			headers: this.HEADERS
 		}, function(error, result, body) {
 			if (!error && result.statusCode === 200)
-				body = JSON.parse(body)
+				body = JSON.parse(body);
 			callback(error, result, body);
-		})
+		});
 	},
 	paymentFields: function() {
 		return ({
@@ -72,13 +70,42 @@ module.exports = {
 			'udf9': '',
 			'udf10': '',
 		});
+	},
+	getRefundFields: function() {
+		return ({
+			"merchantKey": "",
+			"paymentId": "",
+			"refundAmount": ""
+		})
+	},
+	emailInvoiceFields: function() {
+		return ({
+			"customerName": "",
+			"customerEmail": "",
+			"customerPhone": "",
+			"amount": "",
+			"paymentDescription": "",
+			"transactionId": "",
+			"sendEmail": "",
+			"expiryTime": ""
+		});
+	},
+	smsInvoiceFields :function() {
+		return ({		
+				"customerName":"",
+				"customerMobileNumber":"",
+				"amount":"",
+				"description":"",
+				"invoiceReferenceId":"",
+				"confirmSMSPhone":""
+		})
 	},	
 	hashBeforeTransaction: function(data, salt, callback) {
 		var key = "",
 			string = "";
-		var sequence = this.hashSequence.split('|');		
+		var sequence = this.hashSequence.split('|');
 		if (!(data && salt))
-			return "Data and Salt Required";		
+			return "Data and Salt Required";
 		for (var i = 0; i < sequence.length; i++) {
 			key = sequence[i];
 			string += data[key] + '|';
@@ -86,21 +113,21 @@ module.exports = {
 		string += salt;
 		return callback(crypto.createHash('sha512', salt).update(string).digest('hex'));
 	},
-	hashAfterTransaction:function(data, salt, transactionStatus,callback){
+	hashAfterTransaction: function(data, salt, transactionStatus, callback) {
 		var key = "",
 			string = "";
 		var sequence = this.hashSequence.split('|').reverse();
-		if (!(data && salt && transactionStatus)) 
+		if (!(data && salt && transactionStatus))
 			return "Data, Salt, and TransactionStatus Required";
 		string += salt + '|' + transactionStatus + '|';
 		for (var i = 0; i < sequence.length; i++) {
 			key = sequence[i];
 			string += data[key] + '|';
 		}
-		string = string.substr(0, string.length - 1);			
+		string = string.substr(0, string.length - 1);
 		return callback(crypto.createHash('sha512', salt).update(string).digest('hex'));
 	},
-	getPaymentDetails: function(auth, data, callback) {
+	getPaymentDetails: function(data, callback) {
 		var string = "";
 		data.transactionIds.forEach(function(merchantId) {
 			string = merchantId + "|";
@@ -113,13 +140,10 @@ module.exports = {
 			to: data.to,
 			count: data.numberOfPayments
 		});
-		this.setHeaders(auth, Buffer.byteLength(qs), qs);
-		var header = this.HEADERS;
 		var url = BASE_URL + ENDPOINTS.PAYEMENT_DETAILS + qs;
 		this.caller(url, 'POST', header, callback);
-
 	},
-	transactionStatus: function(auth, data, callback) {
+	transactionStatus: function(data, callback) {
 		var string = "";
 		data.transactionIds.forEach(function(merchantId) {
 			string = merchantId + "|";
@@ -129,43 +153,35 @@ module.exports = {
 			merchantKey: data.key,
 			merchantTransactionIds: string
 		});
-		this.setHeaders(auth, Buffer.byteLength(qs), qs);
-		var header = this.HEADERS;
 		var url = BASE_URL + ENDPOINTS.PAYMENT_STATUS + qs;
-		this.caller(url, 'POST', header, callback);
+		this.caller(url, 'POST', callback);
 	},
-	initiateRefund: function(auth, data, callback) {
+	initiateRefund: function(data, callback) {
 		var qs = queryString.stringify({
 			merchantKey: data.key,
 			paymentId: data.paymentId,
 			refundAmount: data.amount
 		});
-		this.setHeaders(auth, Buffer.byteLength(qs), qs);
-		var header = this.HEADERS;
 		var url = BASE_URL + ENDPOINTS.INITIATE_REFUND + qs;
-		this.caller(url, 'POST', header, callback);
+		this.caller(url, 'POST', callback);
 	},
-	paymentRefundDetails: function(auth, data, callback) {
+	paymentRefundDetails: function(data, callback) {
 		var qs = queryString.stringify({
 			merchantKey: data.key,
 			paymentId: data.paymentId
 		});
-		this.setHeaders(auth, Buffer.byteLength(qs), qs);
-		var header = this.HEADERS;
 		var url = BASE_URL + ENDPOINTS.REFUND_DETAILS + qs;
 		this.caller(url, 'POST', header, callback);
 	},
-	refundDetails: function(auth, data, callback) {
+	refundDetails: function(data, callback) {
 		var qs = queryString.stringify({
 			merchantKey: data.key,
 			refundId: data.refundId
 		});
-		this.setHeaders(auth, Buffer.byteLength(qs), qs);
-		var header = this.HEADERS;
 		var url = BASE_URL + ENDPOINTS.REFUND_DETAILS + qs;
 		this.caller(url, 'GET', header, callback);
 	},
-	emailInvoice: function(auth, data, callback) {
+	emailInvoice: function(data, callback) {
 		var qs = queryString.stringify({
 			customerName: data.name,
 			customerEmail: data.email,
@@ -174,14 +190,12 @@ module.exports = {
 			paymentDescription: data.description,
 			transactionId: data.transactionId,
 			sendEmail: data.sendEmail,
-			expiryTime:data.expiry
+			expiryTime: data.expiry
 		})
-		this.setHeaders(auth, Buffer.byteLength(qs), qs);
-		var header = this.HEADERS;
 		var url = BASE_URL + ENDPOINTS.EMAIL_INVOICE + qs;
-		this.caller(url, 'POST', header, callback)
+		this.caller(url, 'POST', callback)
 	},
-	smsInvoice: function(auth, data, callback) {
+	smsInvoice: function(data, callback) {
 		var qs = queryString.stringify({
 			customerName: data.name,
 			customerMobileNumber: data.phone,
@@ -190,9 +204,7 @@ module.exports = {
 			invoiceReferenceId: data.transactionId,
 			confirmSMSPhone: data.confirmPhone
 		});
-		this.setHeaders(auth, Buffer.byteLength(qs), qs);
-		var header = this.HEADERS;
 		var url = BASE_URL + ENDPOINTS.SMS_INVOICE + qs;
-		this.caller(url, 'POST', header, callback);
+		this.caller(url, 'POST', callback);
 	}
 };
